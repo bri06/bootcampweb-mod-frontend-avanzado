7746ecccbc56ce1900238415f242aee0d40a6bd8
Briam <briammartinez@gmail.com>
2019-02-17 20:54:45 +0100

    Solve the problem with the routes of images

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import './styles/index.scss';
 import './js/beers';
 import './js/header';
+import './js/datefilter';
+import './images/bar-209148_640.jpg'
+import './images/bar-209148_1280.jpg'
+import './images/bar-209148_1920.jpg'
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: {}
+            options: { name: '[name].[ext]' }
           }
         ]
       }
